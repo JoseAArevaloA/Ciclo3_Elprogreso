b62348500d033cb62b5fa59dc3c6933b477c42b4
JoseAArevaloA <joseaarevaloa@gmail.com>
2022-09-28 10:10:30 -0500

    bd

--- Ciclo3/settings/production.py
+++ Ciclo3/settings/production.py
@@ -9,11 +9,11 @@ ALLOWED_HOSTS = ['ecommerce-elprogreso.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'El_progreso',
-        'USER': 'usuario_administrador',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd5684eshaufv8b',
+        'USER': 'snkfnlgruqpcip',
+        'PASSWORD': 'c7f3a58a9bb6d196dda0833e4a1c49528f1b5c03934e60c047fe90c01173b8db',
+        'HOST': 'ec2-18-209-78-11.compute-1.amazonaws.com',
+        'PORT': 5432,
     }
 }
