ea2e10d8045ffac9434937b4c78cc3fbef0095f5
JoseAArevaloA <joseaarevaloa@gmail.com>
2022-09-28 09:53:38 -0500

    correcionhost

--- Ciclo3/settings/production.py
+++ Ciclo3/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommerce-elprogreso.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
