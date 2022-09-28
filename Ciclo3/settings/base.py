4a4e210dbb44e4645a6465b6c93830618bcea6cc
JoseAArevaloA <joseaarevaloa@gmail.com>
2022-09-28 10:36:48 -0500

    templates2

--- Ciclo3/settings/base.py
+++ Ciclo3/settings/base.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'Ciclo3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Ciclo3/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
